4b14009fd26b4ebfff0b6cb709c6328504fc50ee
Tamoyaaaaa <7amoyka@gmail.com>
2024-07-03 18:31:53 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _8projtower
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            int couple = 0;
+            int notcouple = 0;
+
+            int i = 0;
+
+            while (i <= number)
+            {
+                if (i % 2 == 0)
+                {
+                    couple++;
+                }
+                else
+                {
+                    notcouple++;
+                }
+                i++;
+
+                Console.WriteLine("Количетсво парных: " + couple);
+                Console.WriteLine("Количество непарных: " + notcouple);
+            }
+        }
+    }
+}
